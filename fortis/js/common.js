af7d1c04c46727122c092c59cdfa9b0f0793af56
Dmitriy <chekanni@gmail.com>
2017-05-16 17:59:20 +0300

    Update common.js

--- fortis/js/common.js
+++ fortis/js/common.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-	/*растягиваем фон на всю ширину блока*/
-	function wResize() {
-		$("header").css("height", $(window).height())
-	};
-	wResize();
-	$(window).resize(function() {
-		wResize()
-	});
-//
+
 	
 });
