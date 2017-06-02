be56bf0bc0ead5222ecd99706ca6d39de3a7666d
Dmitriy <chekanni@gmail.com>
2017-06-02 17:56:04 +0300

    files brend

---
+++ brend/js/common.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+/*	$('.price-item__wrap').slick({
+	infinite: true,
+	dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 3
+	});*/
+	$('.js-review').slick({
+		speed: 1400,
+		nextArrow : ".js-review__next",
+		prevArrow : ".js-review__prew"
+	});
+});
+
+
+
